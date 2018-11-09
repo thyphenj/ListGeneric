3f75fbea9a6b60f5a19fb3bd03ec0ce9f294c955
terence-john davies <terence-john.davies@hanson.biz>
2018-11-09 13:08:10 +0000

    second init commit

---
+++ ListGeneric/ListGeneric/Clue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListGeneric
+{
+    enum EnumDirection { ac, dn };
+
+    partial class Clue
+    {
+        public int Number { get; set; }
+        public EnumDirection Direction { get; set; }
+        public string Text { get; set; }
+        public int Len { get; set; }
+
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+
+        public Clue ()
+        {
+        }
+
+        public Clue(int number, EnumDirection direction, string text, int len, int xPos, int yPos)
+        {
+            Number = number;
+            Direction = direction;
+            Text = text;
+            Len = len;
+            XPos = xPos;
+            YPos = yPos;
+        }
+
+        public override string ToString()
+        {
+            string retval = $"{Number.ToString().PadLeft(2)}{Direction}) {Text} ({Len.ToString()})";
+            return retval;
+        }
+    }
+}
---
+++ ListGeneric/ListGeneric/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Clue> Clues = new List<Clue>();
+
+            Clues.Add(new Clue(2, EnumDirection.ac, "bet", 3, 4, 0));
+            Clues.Add(new Clue(1, EnumDirection.ac, "alf", 4, 0, 0));
+            Clues.Add(new Clue() { Number = 3, Direction = EnumDirection.dn, Text = "del", Len = 3, XPos = 3, YPos = 1 });
+            Clues.Add(new Clue(1, EnumDirection.dn, "gam", 2, 0, 0));
+
+            foreach (Clue clue in Clues.Where(c => c.Direction == EnumDirection.ac).OrderBy(c => c.Number))
+            {
+                Console.WriteLine(clue.ToString());
+            }
+
+            Console.WriteLine();
+
+            foreach (Clue clue in Clues.Where(c => c.Direction == EnumDirection.dn).OrderBy(c => c.Number))
+                Console.WriteLine(clue.ToString());
+        }
+    }
+}
