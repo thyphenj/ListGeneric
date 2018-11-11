54c45f2002ea87a4aa90f4f37a65a4dc5f85eb1f
terence-john davies <t-j@terence-john.co.uk>
2018-11-11 21:54:31 +0000

    Listener 4451

--- ListGeneric/ListGeneric/Program.cs
+++ ListGeneric/ListGeneric/Program.cs
@@ -11,21 +11,29 @@ namespace ListGeneric
             // Populate the Clues
             List<Clue> clues = new List<Clue>
             {
-                new Clue(1, Clue.EnumDir.dn, "gam", 2, 0, 0),
+                new Clue(1, Clue.EnumDir.ac, "alf", 4, 0, 0),
                 new Clue(2, Clue.EnumDir.ac, "bet", 3, 4, 0),
-                new Clue(3, Clue.EnumDir.dn, "del", 3, 3, 1 ),
-                new Clue(1, Clue.EnumDir.ac, "alf", 4, 0, 0)
+
+                new Clue(1, Clue.EnumDir.dn, "gam", 2, 0, 0),
+                new Clue(3, Clue.EnumDir.dn, "del", 3, 3, 1 )
             };
 
             // Populate the Grid
-            Grid grid = new Grid(6, 4);
+            Grid grid = new Grid(5, 4);
 
             foreach (Clue clue in clues)
                 grid.SetClue(clue);
 
-            grid.Cells[2, 2].SetDigit(1);
-            grid.Cells[0, 3].SetDigit(2);
-            grid.Cells[3, 0].SetDigit(3);
+            grid.Cells[1, 1].SetDigit(6);
+            grid.Cells[2, 1].SetDigit(1);
+            grid.Cells[4, 1].SetDigit(3);
+
+            grid.Cells[1, 2].SetDigit(1);
+            grid.Cells[4, 2].SetDigit(6);
+
+            grid.Cells[1, 3].SetDigit(3);
+            grid.Cells[3, 3].SetDigit(6);
+            grid.Cells[4, 3].SetDigit(1);
 
             //----------------------------- report on contents!
             // The clues
